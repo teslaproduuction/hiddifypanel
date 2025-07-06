54dbc834204cf5e10bd98d020e4484993e7be693
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2025-07-06 19:30:11 +0330

    release: version 10.86.0b1 🚀

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # import importlib.metadata
 from datetime import datetime
 
-__version__ = '10.85.0b21'
-__release_time__= datetime.strptime('2025-07-03T11:00:05','%Y-%m-%dT%H:%M:%S')
+__version__ = '10.86.0b1'
+__release_time__= datetime.strptime('2025-07-06T19:30:06','%Y-%m-%dT%H:%M:%S')
 is_released_version = False
